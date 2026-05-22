c9dc39ce70ee0f9e95ac25adfd170ae9af3d1eaf
Richa Rai <richarai9919@gmail.com>
2026-05-22 23:31:15 +0530

    Update: 98. Validate Binary Search Tree (C++)

--- neetcode150/Tree/Medium/98-validate-binary-search-tree.cpp
+++ neetcode150/Tree/Medium/98-validate-binary-search-tree.cpp
@@ -2,7 +2,7 @@
 // Problem: 98. Validate Binary Search Tree
 // Difficulty: Medium
 // Topics: Tree, Depth-First Search, Binary Search Tree, Binary Tree
-// Runtime: 2 ms (Beats 6.4%)
+// Runtime: 0 ms (Beats 100.0%)
 // Memory: 21.9 MB (Beats 43.4%)
 // Submitted: May 22, 2026
 // Link: https://leetcode.com/problems/validate-binary-search-tree/
@@ -22,16 +22,16 @@
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        return valid(root, LONG_MIN, LONG_MAX);
+        return validBST(root, LONG_MIN, LONG_MAX);
     }
 
-    bool valid(TreeNode* root, long left, long right){
+    bool validBST(TreeNode* root, long left, long right){
         if(!root) return true;
 
         if(!(root->val > left && root->val < right)){
             return false;
         }else{
-            return valid(root->left, left, root->val) && valid(root->right, root->val, right);
+            return validBST(root->left, left, root->val) && validBST(root->right, root->val, right);
         }
     }
 };
